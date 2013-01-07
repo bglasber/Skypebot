2bb05ef096e79e84ccfd2f862159025d16d7a82f
Brandon <beanerbut2@gmail.com>
2013-01-07 01:41:24 -0500

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -1,1 +1,27 @@
+def pluralize(word):
+    word = word.strip()
 
+    #irregular cases
+    if word == "goose":
+        word = "geese"
+
+    elif word == "mouse":
+        word = "mice"
+
+    elif word == "knife"
+        word = "knives"
+
+    elif word == "wolf"
+        word = "wolves"
+        
+    else:
+        if word.endswith("y"):
+            word = word[:-1] + "ies"
+
+        if word.endswith("s"):
+            word = word[:-1] + "es"
+            
+        else:
+            word += "s"
+        
+    return word
