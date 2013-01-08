fb90f670f483ac532795e054ac5a73c948ba83c6
Brandon <beanerbut2@gmail.com>
2013-01-08 17:57:56 -0500

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -1,68 +1,70 @@
-# noun contexts
-def pluralize(word):
-    word = word.strip()
-
-    # irregular cases
-    if word == "goose":
-        word = "geese"
-
-    elif word == "mouse":
-        word = "mice"
-
-    elif word == "sheep"
-        word = "sheep"
-
-    elif word == "moose"
-        word = "moose"
-
-    elif word == "man"
-        word = "men"
-
-    elif word == "woman"
-        word = "women"
-
-    elif word == "person"
-        word = "people"
-        
-    else:
-        if word.endswith("y"):
-            word = word[:-1] + "ies"
-
-        if word.endswith("s"):
-            word = word[:-1] + "es"
-            
-        if word.endswith("f"):
-            word = word[:-1] + "ves"
-
-        if word.endswith("fe"):
-            word = word[:-2] + "ves"
-            
-        if word.endswith("us"):
-            word = word[:-2] + "i"
+class Grammar:
+    
+    # noun contexts
+    def pluralize(word):
+        word = word.strip()
+    
+        # irregular cases
+        if word == "goose":
+            word = "geese"
+    
+        elif word == "mouse":
+            word = "mice"
+    
+        elif word == "sheep"
+            word = "sheep"
+    
+        elif word == "moose"
+            word = "moose"
+    
+        elif word == "man"
+            word = "men"
+    
+        elif word == "woman"
+            word = "women"
+    
+        elif word == "person"
+            word = "people"
             
         else:
-            word += "s"
-        
-    return word
+            if word.endswith("y"):
+                word = word[:-1] + "ies"
     
-
-
-# verb contexts
-def ing(word):
-    word = word.strip()
+            if word.endswith("s"):
+                word = word[:-1] + "es"
+                
+            if word.endswith("f"):
+                word = word[:-1] + "ves"
     
-    # irregular cases
-    if word = "":
-        word = ""
+            if word.endswith("fe"):
+                word = word[:-2] + "ves"
+                
+            if word.endswith("us"):
+                word = word[:-2] + "i"
+                
+            else:
+                word += "s"
+            
+        return word
         
-    else:
-        if word.endswith("ie"):
-            word = word[:-2] + "ying"
+    
+    
+    # verb contexts
+    def ing(word):
+        word = word.strip()
         
-        if word.endswith("ot"):
-            word = word[:-2] + "eting"
+        # irregular cases
+        if word = "":
+            word = ""
             
         else:
-            word += "ing"
-    
-    return word
+            if word.endswith("ie"):
+                word = word[:-2] + "ying"
+            
+            if word.endswith("ot"):
+                word = word[:-2] + "eting"
+                
+            else:
+                word += "ing"
+        
+        return word
