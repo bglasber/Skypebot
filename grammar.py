6e0889f5b8e2f43b59172861cb64e6c834033cb7
Brandon <beanerbut2@gmail.com>
2013-01-08 19:11:57 -0500

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -11,60 +11,40 @@ class Grammar:
         elif word.lower() == "mouse":
             word = "mice"
     
-        elif word.lower() == "sheep"
+        elif word.lower() == "sheep":
             word = "sheep"
     
-        elif word.lower() == "moose"
+        elif word.lower() == "moose":
             word = "moose"
     
-        elif word.lower() == "man"
+        elif word.lower() == "man":
             word = "men"
     
-        elif word.lower() == "woman"
+        elif word.lower() == "woman":
             word = "women"
     
-        elif word.lower() == "person"
+        elif word.lower() == "person":
             word = "people"
+
+        elif word.lower() == "die":
+            word = "dice"
             
         else:
-            if word.lower().endswith("y"):
-                word = word[:-1] + "ies"
-    
-            if word.lower().endswith("s"):
-                word = word[:-1] + "es"
-                
-            if word.lower().endswith("f"):
-                word = word[:-1] + "ves"
-    
             if word.lower().endswith("fe"):
                 word = word[:-2] + "ves"
                 
-            if word.lower().endswith("us"):
+            elif word.lower().endswith("us"):
                 word = word[:-2] + "i"
                 
+            elif word.lower().endswith("y"):
+                word = word[:-1] + "ies"
+    
+            elif word.lower().endswith("s"):
+                word = word + "es"
+                
+            elif word.lower().endswith("f"):
+                word = word[:-1] + "ves"
             else:
                 word += "s"
             
-        return word
-        
-    
-    
-    # verb contexts
-    def ing(word):
-        word = word.strip()
-        
-        # irregular cases
-        #if word.lower() = "":
-            #word = ""
-            
-        else:
-            if word.lower().endswith("ie"):
-                word = word[:-2] + "ying"
-            
-            if word.lower().endswith("ot"):
-                word = word[:-2] + "eting"
-                
-            else:
-                word += "ing"
-        
         return word
