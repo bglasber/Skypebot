d618fc72fb9aa2b7f3fc73443bd4dcbbfc33bdff
Brandon <beanerbut2@gmail.com>
2013-01-08 21:06:41 -0500

    Update lib/commandHandlers.py

--- lib/commandHandlers.py
+++ lib/commandHandlers.py
@@ -107,7 +107,7 @@ def videoURLHandler(msg):
         x += 1
     
     c = Command(None)
-    c.saveVideoURL(url,msg.FromDisplayName)
+    c.insertLink(msg.FromDisplayName, url, "VIDEO")
             
 def randomVideoHandler(msg):
     """Display a random youtube video URL"""
