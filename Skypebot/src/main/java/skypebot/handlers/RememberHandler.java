a9517c3edd2934b602de504210df020147908cbc
Brad Glasbergen <bglasber@gmail.com>
2013-10-17 19:29:59 -0400

    Adds Stuff for quote

--- Skypebot/src/main/java/skypebot/handlers/RememberHandler.java
+++ Skypebot/src/main/java/skypebot/handlers/RememberHandler.java
@@ -2,6 +2,7 @@ package skypebot.handlers;
 
 import com.skype.ChatMessage;
 import com.skype.SkypeException;
+import org.apache.log4j.Logger;
 import skypebot.db.IDbManager;
 import skypebot.engine.Engine;
 
@@ -15,6 +16,7 @@ import java.sql.SQLException;
 public class RememberHandler implements IHandler {
 
     private IDbManager manager;
+    private static Logger logger = Logger.getLogger( RememberHandler.class.getCanonicalName() );
 
     @Override
     public boolean canHandle( ChatMessage m ) {
@@ -34,21 +36,26 @@ public class RememberHandler implements IHandler {
     @Override
     public void handle( ChatMessage m ) {
         try {
+            logger.debug( "RememberHandler will be handling the message" );
             String[] fields = m.getContent().replace(
                 "bucket, remember ([^ ]+) \"([^\"])\" ([0-9]+)",
                 "$1@$2@$3"
             ).split( "@" );
-            String alias = manager.getSingleFromDbThatEquals(
+
+            logger.trace( "Unaliasing: " + fields[ 0 ] );
+            String realName = manager.getSingleFromDbThatEquals(
                 manager.getSchema().getAliasTable(),
-                "realId",
                 "alias",
+                "realId",
                 fields[ 0 ]
             );
+            logger.debug( "Found Real Name: " + realName );
             String message = Engine.messageList.GetMessageToQuote(
-                alias,
+                realName,
                 fields[ 1 ],
                 Integer.parseInt( fields[ 2 ] )
             );
+            logger.debug( "Got message: " + message );
 
             manager.insertFieldsIntoTable(
                 manager.getSchema().getQuotesTable(),
